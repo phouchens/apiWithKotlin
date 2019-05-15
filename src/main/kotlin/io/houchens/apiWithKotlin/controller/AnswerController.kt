ba8082b726d7730342f583ca95c3226f0062a9eb
Perry <perry.houchens@gmail.com>
2019-05-14 21:03:54 -0400

    Adds Swagger

--- src/main/kotlin/io/houchens/apiWithKotlin/controller/AnswerController.kt
+++ src/main/kotlin/io/houchens/apiWithKotlin/controller/AnswerController.kt
@@ -2,6 +2,8 @@ package io.houchens.apiWithKotlin.controller
 
 import io.houchens.apiWithKotlin.Model.Answer
 import io.houchens.apiWithKotlin.exception.ResourceNotFoundException
+import io.houchens.apiWithKotlin.repository.AnswerRepository
+import io.houchens.apiWithKotlin.repository.QuestionRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -11,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import io.houchens.apiWithKotlin.repository.AnswerRepository
-import io.houchens.apiWithKotlin.repository.QuestionRepository
 import javax.validation.Valid
 
 @RestController
@@ -20,9 +20,10 @@ class AnswerController(
     private val answerRepository: AnswerRepository,
     private val questionRepository: QuestionRepository
 ) {
-
+    
     @GetMapping("/questions/{questionId}/answers")
-    fun getAnswerByQuestionId(@PathVariable questionId: Long): List<Answer> = answerRepository.findByQuestionId(questionId)
+    fun getAnswerByQuestionId(@PathVariable questionId: Long): List<Answer> =
+        answerRepository.findByQuestionId(questionId)
 
     @PostMapping("/questions/{questionId}/answers")
     fun addAnswer(@PathVariable questionId: Long, @Valid @RequestBody answer: Answer): Answer {
@@ -34,9 +35,10 @@ class AnswerController(
     }
 
     @PutMapping("/questions/{questionId}/answers/{answerId}")
-    fun updateAnswer(@PathVariable questionId: Long, @PathVariable answerId: Long, @Valid @RequestBody answerRequest: Answer
+    fun updateAnswer(
+        @PathVariable questionId: Long, @PathVariable answerId: Long, @Valid @RequestBody answerRequest: Answer
     ): Answer {
-        if (!questionRepository.existsById(questionId)) throw ResourceNotFoundException("Could not find question $questionId"       )
+        if (!questionRepository.existsById(questionId)) throw ResourceNotFoundException("Could not find question $questionId")
         return answerRepository.findById(answerId)
             .map { answer ->
                 answer.text = answerRequest.text
